00acfd10ac1702fe2bfa460ebc4bab5c79367bf2
so0112 <tkddh0112@gmail.com>
2022-09-20 09:34:07 +0900

    feat: Nav 제작

---
+++ client/src/components/navigation/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import NavFrame from "./style";
+
+function Nav() {
+  return (
+    <NavFrame>
+      <div className="logo">
+        <div>logo</div>
+        <div className="left">
+          <div>전체</div>
+          <div>스터디</div>
+          <div>프로젝트</div>
+        </div>
+      </div>
+      <div className="right">
+        <div>작성하기</div>
+        <div>로그인</div>
+      </div>
+    </NavFrame>
+  );
+}
+
+export default Nav;
