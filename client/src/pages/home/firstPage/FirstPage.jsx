d3a8d850308d291a74cff151ca27a74f1da3b46e
so0112 <tkddh0112@gmail.com>
2022-09-20 16:55:39 +0900

    Modify: Nav, FristPage, SecondPage 파일 변경

--- client/src/pages/home/firstPage/FirstPage.jsx
+++ client/src/pages/home/firstPage/FirstPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import FirstPageStyle from "./style";
+import styled from "styled-components";
 import Video from "../../../assets/videos/landingVideo.mp4";
 
 function FirstPage() {
@@ -15,8 +15,9 @@ function FirstPage() {
           <div>어디에서 스터디를 구하고 프로젝트를 개설하실지</div>
           <div>막막하시다면 수풀에서 찾아보세요!</div>
         </div>
-
-        <div>버튼 컴포넌트 자리</div>
+        <StartSoopool>
+          <a href="/">시작하기</a>
+        </StartSoopool>
       </div>
 
       <div className="video-section">
@@ -32,5 +33,67 @@ function FirstPage() {
     </FirstPageStyle>
   );
 }
+
+const FirstPageStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  height: 95vh;
+  position: relative;
+
+  .video-section {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  .landing-video {
+    height: 100%;
+    width: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
+    opacity: 0.4;
+  }
+
+  .content-layout {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    height: 80%;
+    z-index: 2;
+
+    h1 {
+      font-size: 80px;
+      color: black;
+      white-space: nowrap;
+    }
+
+    .body {
+      margin-top: 100px;
+      margin-bottom: 100px;
+      font-size: 30px;
+      color: black;
+    }
+  }
+`;
+
+const StartSoopool = styled.div`
+  display: flex;
+  > a {
+    color: white;
+    text-decoration: none;
+    padding: 1rem 2.5rem;
+    background-color: #66bd6c;
+    margin-top: 3rem;
+    border-radius: 40px;
+  }
+`;
 
 export default FirstPage;
