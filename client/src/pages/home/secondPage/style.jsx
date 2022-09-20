23a953606a28e08c2e38dba4fb2a0207813387fc
so0112 <tkddh0112@gmail.com>
2022-09-20 12:25:09 +0900

    Feat: landing second 추가

---
+++ client/src/pages/home/secondPage/style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const SecondPageStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  height: 95vh;
+  position: relative;
+  z-index: 1;
+`;
+
+export default SecondPageStyle;
