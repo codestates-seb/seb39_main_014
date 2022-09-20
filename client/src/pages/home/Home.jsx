8b33fa4b89b23dcc01944771e43f5d7c69acaa1d
hopak-e <a328028@gmail.com>
2022-09-20 11:51:09 +0900

    Feat:3번째 랜딩페이지 작성

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FirstPage from "./firstPage/FirstPage";
+import ThirdPage from "./thirdPage/ThirdPage";
+import LastPage from "./lastPage/LastPage";
+
+function Home() {
+  return (
+    <div>
+      <FirstPage />
+      <ThirdPage />
+      <LastPage />
+    </div>
+  );
+}
+
+export default Home;
