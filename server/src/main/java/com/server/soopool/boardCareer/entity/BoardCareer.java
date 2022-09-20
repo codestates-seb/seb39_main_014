cf199f5d87c368f3c72b3b7fe52d769b7d08290a
euncheol-kim <klmeuncheol@kakao.com>
2022-09-20 13:15:37 +0900

    Add : ãBoard 엔티íBoardCareer± 및 관êê엔티ã생ë 및 엔티티 관계설정

--- server/src/main/java/com/server/soopool/boardCareer/entity/BoardCareer.java
+++ server/src/main/java/com/server/soopool/boardCareer/entity/BoardCareer.java
@@ -1,4 +1,43 @@
 package com.server.soopool.boardCareer.entity;
 
+import com.server.soopool.board.entity.Board;
+import com.server.soopool.career.entity.Career;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * 작성자 : 김은철
+ * 작성핵심 : Entity 연관관계 매핑, 양방향 또는 단방향의 정의
+ * 작성일자 : 2022-09-19
+ * 최신 수정자 :
+ * 최신 수정일자 :
+ * email : klmeuncheol@kakao.com
+ */
+@Entity
+@Getter
+@Setter
 public class BoardCareer {
+    //Todo : 컬럼의 제약조건 & 레이지 로딩 설정하기
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //ManyToOne 컬럼설정
+    @ManyToOne(targetEntity = Board.class)
+    @JoinColumn(name = "board_id")
+    private Board boardId;
+
+    @ManyToOne(targetEntity = Career.class)
+    @JoinColumn(name = "career_id")
+    private Career careerId;
+
+    //기본컬럼 설정
+    @Column
+    private Integer careerTotalRecruit;
+
+    @Column
+    private Integer careerCurrentRecruit;
+
 }
