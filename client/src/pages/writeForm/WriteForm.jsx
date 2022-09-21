6e9bc9df73cef0f28c1730aaf9e97f072c581604
hopak-e <a328028@gmail.com>
2022-09-22 00:17:29 +0900

    Design: 반응형 수정

--- client/src/pages/writeForm/WriteForm.jsx
+++ client/src/pages/writeForm/WriteForm.jsx
@@ -1,86 +1,112 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   WriteFormContainer,
   FormContainer,
-  ContentDivision,
-  ContentLeft,
-  ContentRight,
-  Career,
+  FirstDivision,
+  FirstLeft,
+  FirstRight,
+  SecondLeft,
+  SecondRight,
   Contact,
   Title,
   Content,
+  SecondDivision,
 } from "./styled";
 import CkEditor from "../../components/ckEditor/CKEditor";
+import { stackLists, regionLists, periodLists } from "./WriteFormData";
+import CareerForm from "./CareerForm";
 
 function WriteForm() {
+  const [state, setState] = useState({
+    editor: null,
+  });
+  const [isMethod, setIsMethod] = useState("study");
+  const [isChecked, setIsChecked] = useState("online");
+
+  useEffect(() => {
+    const editor = <CkEditor />;
+    setState({ ...state, editor: editor });
+  }, []);
+
+  const onMethodChangeHandler = (e) => {
+    setIsMethod(e.target.value);
+  };
+
+  const onRecruitmentHandler = (e) => {
+    setIsChecked(e.target.value);
+  };
+
   return (
     <WriteFormContainer>
       <FormContainer>
-        <ContentDivision>
-          <ContentLeft>
+        <FirstDivision>
+          <FirstLeft>
             <label htmlFor="repo">모집 구분</label>
             <div className="Check-box">
-              <input id="repo" type="radio" />
-              <span>스터디</span>
-              <input id="repo" type="radio" />
-              <span>프로젝트</span>
+              <input
+                id="study"
+                type="radio"
+                value="study"
+                checked={isMethod === "study"}
+                onChange={onMethodChangeHandler}
+              />
+              <label htmlFor="study">스터디</label>
+              <input
+                id="project"
+                type="radio"
+                value="project"
+                checked={isMethod === "project"}
+                onChange={onMethodChangeHandler}
+              />
+              <label htmlFor="project">프로젝트</label>
             </div>
-            <label htmlFor="classification">기술 스택</label>
-            <select id="classification">
-              <option>프로젝트 사용 스택</option>
-              <option>java</option>
-              <option>javascript</option>
-              <option>react</option>
-              <option>node</option>
-            </select>
-          </ContentLeft>
-          <ContentRight>
-            <label htmlFor="repo">모임 방식</label>
+          </FirstLeft>
+          <FirstRight>
+            <label htmlFor="online">모임 방식</label>
             <div className="Check-box">
-              <input id="repo" type="radio" />
-              <span>온라인</span>
-              <input id="repo" type="radio" />
-              <span>오프라인</span>
+              <input
+                id="online"
+                type="radio"
+                value="online"
+                checked={isChecked === "online"}
+                onChange={onRecruitmentHandler}
+              />
+              <label htmlFor="online">온라인</label>
+              <input
+                id="offline"
+                type="radio"
+                value="offline"
+                checked={isChecked === "offline"}
+                onChange={onRecruitmentHandler}
+              />
+              <label htmlFor="offline">오프라인</label>
               <select id="region">
-                <option>지역</option>
-                <option>서울</option>
-                <option>인천</option>
-                <option>경기도</option>
-                <option>강원도</option>
-                <option>경상도</option>
-                <option>전라도</option>
-                <option>충청도</option>
+                {regionLists.map((el) => (
+                  <option key={el.id}>{el.region}</option>
+                ))}
               </select>
             </div>
+          </FirstRight>
+        </FirstDivision>
+        <SecondDivision>
+          <SecondLeft>
+            <label htmlFor="classification">기술 스택</label>
+            <select id="classification">
+              {stackLists.map((el) => (
+                <option key={el.id}>{el.stack}</option>
+              ))}
+            </select>{" "}
+          </SecondLeft>
+          <SecondRight>
             <label htmlFor="period">기간</label>
             <select id="period">
-              <option>1개월</option>
-              <option>2개월</option>
-              <option>3개월</option>
-              <option>4개월</option>
-              <option>5개월</option>
-              <option>6개월</option>
-              <option>장기</option>
+              {periodLists.map((el) => (
+                <option key={el.id}>{el.period}</option>
+              ))}
             </select>
-          </ContentRight>
-        </ContentDivision>
-        <Career>
-          <label htmlFor="categorization">모집 분류 / 인원</label>
-          <div className="Bundle">
-            <select id="categorization">
-              <option>웹 프론트엔드</option>
-              <option>웹 백엔드</option>
-              <option>모바일</option>
-              <option>웹 디자이너</option>
-              <option>기타</option>
-            </select>
-            <div>- 1 +</div>
-            <button type="button">추가</button>
-            <button type="button" className="Delete">
-              삭제
-            </button>
-          </div>
-        </Career>
+          </SecondRight>
+        </SecondDivision>
+        <CareerForm />
         <Contact>
           <label htmlFor="input">연락 방법</label>
           <input
@@ -95,7 +121,7 @@ function WriteForm() {
         </Title>
         <Content>
           <label htmlFor="input">내용</label>
-          <CkEditor />
+          {state.editor}
         </Content>
       </FormContainer>
     </WriteFormContainer>
