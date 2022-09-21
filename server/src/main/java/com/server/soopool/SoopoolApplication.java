9e03537cef9f33268e237757cd08bdae342a7539
yeojoo1224 <yeojoo1224@naver.com>
2022-09-21 18:04:06 +0900

    Modify: JPA Auditing 활성화 애노테이션 추가

--- server/src/main/java/com/server/soopool/SoopoolApplication.java
+++ server/src/main/java/com/server/soopool/SoopoolApplication.java
@@ -3,8 +3,11 @@ package com.server.soopool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+// JPA Auditing 활성화
+@EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SoopoolApplication {
 
