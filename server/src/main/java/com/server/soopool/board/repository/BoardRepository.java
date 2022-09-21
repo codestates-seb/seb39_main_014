91473be0b2ef61ae78e56f5fc8e0aa2faf9c834d
yeojoo1224 <yeojoo1224@naver.com>
2022-09-21 18:18:32 +0900

    Add: BoardRepository 추가

--- server/src/main/java/com/server/soopool/board/repository/BoardRepository.java
+++ server/src/main/java/com/server/soopool/board/repository/BoardRepository.java
@@ -2,6 +2,12 @@ package com.server.soopool.board.repository;
 
 import com.server.soopool.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Optional<Board> findById(Long id);
 }
