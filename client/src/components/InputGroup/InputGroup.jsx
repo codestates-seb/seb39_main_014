7048c5104d240726b1ed7be41ff271477139e260
so0112 <tkddh0112@gmail.com>
2022-09-21 09:35:13 +0900

    Feat: Login 컴포넌트 제작

--- client/src/components/InputGroup/InputGroup.jsx
+++ client/src/components/InputGroup/InputGroup.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 function InputGroup() {
   return (
-    <div>
+    <InputLayout>
       <div>
-        <input />
-        <small />
+        <input className="input-area" placeholder />
+        <small> </small>
       </div>
-    </div>
+    </InputLayout>
   );
 }
+
+const InputLayout = styled.div`
+  margin-top: 5px;
+  .input-area {
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    width: 100%;
+    height: 48px;
+    appearance: none;
+    padding: 22px 12px 10px;
+    min-width: 300px;
+  }
+`;
 
 export default InputGroup;
