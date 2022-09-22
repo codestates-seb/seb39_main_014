a61f338d4ee74f6daaacbaf5fa635cb57237f8b3
hopak-e <a328028@gmail.com>
2022-09-22 17:18:12 +0900

    Feat:취소 버튼 구현

--- client/src/pages/writeForm/WriteFormData.jsx
+++ client/src/pages/writeForm/WriteFormData.jsx
@@ -1,41 +1,41 @@
 export const stackLists = [
-    { id: 0, stack: "- 프로젝트 사용 스택 -" },
-    { id: 1, stack: "Java" },
-    { id: 2, stack: "JavaScript" },
-    { id: 3, stack: "React" },
-    { id: 4, stack: "Node" },
-    { id: 5, stack: "Next" },
-    { id: 6, stack: "Express" },
-    { id: 7, stack: "Vue" },
-    { id: 8, stack: "TypeScript" },
-  ];
-  
-  export const regionLists = [
-    { id: 1, region: "서울" },
-    { id: 2, region: "인천" },
-    { id: 3, region: "경기도" },
-    { id: 4, region: "강원도" },
-    { id: 5, region: "경상도" },
-    { id: 6, region: "전라도" },
-    { id: 7, region: "충청도" },
-    { id: 8, region: "제주도" },
-  ];
-  
-  export const periodLists = [
-    { id: 1, period: "미정" },
-    { id: 2, period: "1개월" },
-    { id: 3, period: "2개월" },
-    { id: 4, period: "3개월" },
-    { id: 5, period: "4개월" },
-    { id: 6, period: "5개월" },
-    { id: 7, period: "6개월" },
-    { id: 8, period: "장기" },
-  ];
-  
-  export const careerLists = [
-    { id: 1, career: "웹 프론트엔드" },
-    { id: 2, career: "웹 백엔드" },
-    { id: 3, career: "웹 디자이너" },
-    { id: 4, career: "모바일" },
-    { id: 5, career: "기타" },
-  ];
+  { id: 0, stack: "- 프로젝트 사용 스택 -" },
+  { id: 1, stack: "Java" },
+  { id: 2, stack: "JavaScript" },
+  { id: 3, stack: "React" },
+  { id: 4, stack: "Node" },
+  { id: 5, stack: "Next" },
+  { id: 6, stack: "Express" },
+  { id: 7, stack: "Vue" },
+  { id: 8, stack: "TypeScript" },
+];
+
+export const regionLists = [
+  { id: 1, region: "서울" },
+  { id: 2, region: "인천" },
+  { id: 3, region: "경기도" },
+  { id: 4, region: "강원도" },
+  { id: 5, region: "경상도" },
+  { id: 6, region: "전라도" },
+  { id: 7, region: "충청도" },
+  { id: 8, region: "제주도" },
+];
+
+export const periodLists = [
+  { id: 1, period: "미정" },
+  { id: 2, period: "1개월" },
+  { id: 3, period: "2개월" },
+  { id: 4, period: "3개월" },
+  { id: 5, period: "4개월" },
+  { id: 6, period: "5개월" },
+  { id: 7, period: "6개월" },
+  { id: 8, period: "장기" },
+];
+
+export const careerLists = [
+  { id: 1, career: "웹 프론트엔드" },
+  { id: 2, career: "웹 백엔드" },
+  { id: 3, career: "웹 디자이너" },
+  { id: 4, career: "모바일" },
+  { id: 5, career: "기타" },
+];
--- client/src/pages/writeForm/styled.jsx
+++ client/src/pages/writeForm/styled.jsx
@@ -4,7 +4,7 @@ export const WriteFormContainer = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-  height: 95vh;
+  height: 100vh;
 `;
 
 export const FormContainer = styled.form`
@@ -19,11 +19,13 @@ export const FormContainer = styled.form`
   }
 `;
 
+/* Division */
+
 export const FirstDivision = styled.div`
   display: flex;
   justify-content: space-between;
   width: 100%;
-  @media screen and (max-width: 687px) {
+  @media screen and (max-width: 710px) {
     flex-direction: column;
   }
 `;
@@ -41,9 +43,12 @@ export const FirstLeft = styled.div`
       border-radius: 50%;
       width: 1.5rem;
       height: 1.5rem;
-      margin: 0 1rem 0 0.2rem;
+      margin: 0.5rem 1rem 0.5rem 0.2rem;
       accent-color: green;
       cursor: pointer;
+      @media screen and (max-width: 710px) {
+        margin-left: 0;
+      }
     }
     > label {
       font-weight: 400;
@@ -66,7 +71,7 @@ export const FirstRight = styled.div`
     > input {
       width: 1.5rem;
       height: 1.5rem;
-      margin: 0 1rem 0 0;
+      margin: 0.5rem 1rem 0.5rem 0;
       cursor: pointer;
       &:checked {
         accent-color: green;
@@ -76,7 +81,7 @@ export const FirstRight = styled.div`
 
     > select {
       height: 1.5rem;
-      width: 25%;
+      width: 23%;
       margin-left: 2rem;
       padding-left: 0.5rem;
     }
@@ -84,7 +89,11 @@ export const FirstRight = styled.div`
     > label {
       font-weight: 400;
       font-size: 1.1rem;
-      width: 4rem;
+      width: 4.5rem;
+      @media screen and (max-width: 710px) {
+        width: 4rem;
+        margin-right: 1.8rem;
+      }
     }
   }
 
@@ -93,7 +102,7 @@ export const FirstRight = styled.div`
     height: 2.3rem;
     width: 40%;
   }
-  @media screen and (max-width: 687px) {
+  @media screen and (max-width: 710px) {
     padding-left: 0;
   }
 `;
@@ -103,7 +112,7 @@ export const SecondDivision = styled.div`
   justify-content: space-between;
   width: 100%;
   margin-bottom: 1rem;
-  @media screen and (max-width: 687px) {
+  @media screen and (max-width: 710px) {
     flex-direction: column;
   }
 `;
@@ -113,11 +122,11 @@ export const SecondLeft = styled.div`
   flex-direction: column;
   width: 100%;
   padding-right: 0.5rem;
-  @media screen and (max-width: 687px) {
+  @media screen and (max-width: 710px) {
     padding-right: 0rem;
   }
   > select {
-    margin: 0.5rem 0 1rem 0;
+    margin: 1rem 0 0.5rem 0;
     height: 2.3rem;
     width: 100%;
     padding-left: 0.5rem;
@@ -130,34 +139,32 @@ export const SecondRight = styled.div`
   width: 100%;
   padding-left: 0.5rem;
   > select {
-    margin: 0.5rem 0 1rem 0;
+    margin: 1rem 0 0.5rem 0;
     height: 2.3rem;
     width: 100%;
     padding-left: 0.5rem;
   }
-  @media screen and (max-width: 687px) {
+  @media screen and (max-width: 710px) {
     padding-left: 0;
   }
 `;
+
+/* 모집 분류 / 인원 */
 
 export const Career = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  margin-bottom: 1rem;
   .Bundle {
     display: flex;
     align-items: center;
-    margin: 0.5rem 0 1rem 0;
+    margin: 1rem 0 1rem 0;
     > select {
       height: 2.3rem;
       width: 49%;
       padding-left: 0.5rem;
     }
-
-    /* .Delete {
-      background-color: #ffe3e4;
-      color: #ff7d85;
-    } */
   }
   .BundleButton {
     display: flex;
@@ -172,11 +179,11 @@ export const Career = styled.div`
       display: flex;
       align-items: center;
       justify-content: center;
-      width: 2rem;
-      height: 2rem;
+      width: 1.8rem;
+      height: 1.8rem;
       margin: 0 1rem;
-      color: black;
-      border: 1px solid black;
+      color: #595959;
+      border: 1px solid #595959;
       border-radius: 50%;
       background-color: white;
       font-size: 1.5rem;
@@ -202,13 +209,41 @@ export const Career = styled.div`
   }
 `;
 
+export const Crew = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.6rem;
+  width: 100%;
+  > div {
+    width: 49%;
+    font-size: 0.9rem;
+    font-weight: 400;
+    padding: 0.5rem;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+  > button {
+    width: 5rem;
+    margin-left: 1rem;
+    padding: 0.7rem 1rem;
+    height: 2.2rem;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    background-color: #ffe3e4;
+    color: #ff7d85;
+  }
+`;
+
+/* 연락 방법 */
+
 export const Contact = styled.div`
   display: flex;
   flex-direction: column;
 
   > input {
     width: 49%;
-    @media screen and (max-width: 687px) {
+    @media screen and (max-width: 710px) {
       width: 100%;
     }
     padding: 0.5rem 0;
@@ -248,3 +283,27 @@ export const Content = styled.div`
   flex-direction: column;
 `;
 
+export const PostButton = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 1rem 0;
+  .Post {
+    width: 15%;
+    max-width: 110px;
+    margin-left: 1rem;
+    margin-bottom: 1rem;
+    padding: 0.7rem 1rem;
+    border-radius: 5px;
+    font-weight: 500;
+    border: none;
+    cursor: pointer;
+    &.cancel {
+      background-color: #e4e4e4;
+    }
+
+    &.complete {
+      background-color: #e5f8ed;
+      color: #009f47;
+    }
+  }
+`;
