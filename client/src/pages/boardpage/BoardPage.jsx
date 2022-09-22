70e0d799c87a1c4d8e4d5d46be18ace703c24945
so0112 <tkddh0112@gmail.com>
2022-09-22 17:25:41 +0900

    Modify: json server 파일명 변경, npm run json 명령어 추가

--- client/src/pages/boardpage/BoardPage.jsx
+++ client/src/pages/boardpage/BoardPage.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
 function BoardPage() {
-  return <div>BoardPage</div>;
+  return <BoardPageLayout>BoardPage</BoardPageLayout>;
 }
+
+const BoardPageLayout = styled.div`
+  background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80vh;
+`;
+
+const LeftSide = styled.div`
+  background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80vh;
+`;
+
+const RightSide = styled.div`
+  background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80vh;
+`;
 
 export default BoardPage;
