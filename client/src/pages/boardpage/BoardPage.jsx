2105d0290f1bb7b03718b3c252c29bfe9f0a855c
so0112 <tkddh0112@gmail.com>
2022-09-23 21:07:32 +0900

    Feat: Board stack 컴포넌트 구현

--- client/src/pages/boardpage/BoardPage.jsx
+++ client/src/pages/boardpage/BoardPage.jsx
@@ -1,35 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import Footer from "../../components/footer/Footer";
+import Board from "../../components/board/Board";
+import Stack from "../../components/stack/Stack";
+import axios from "axios";
 
 function BoardPage() {
-  return <BoardPageLayout>BoardPage</BoardPageLayout>;
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/board/").then((res) => {
+      setDatas(res.data);
+    });
+  }, []);
+
+  console.log(datas);
+  return (
+    <>
+      <BoardPageLayout>
+        <Side />
+        <Center>
+          <StackArea>
+            <Stack />
+          </StackArea>
+          <Content>
+            {datas.map((data) => (
+              <Board data={data} key={data.board_id} />
+            ))}
+          </Content>
+          <PageNationArea>페이지네이션 자리</PageNationArea>
+        </Center>
+        <Side />
+      </BoardPageLayout>
+      <Footer />
+    </>
+  );
 }
 
 const BoardPageLayout = styled.div`
-  background-color: gray;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
   width: 100%;
-  height: 80vh;
 `;
 
-const LeftSide = styled.div`
-  background-color: gray;
+const StackArea = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 80vh;
+  justify-content: center;
 `;
 
-const RightSide = styled.div`
-  background-color: gray;
+const Center = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 10fr 1fr;
+`;
+
+const Content = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  background-color: white;
+  min-width: 1280px;
+
+  @media screen and (max-width: 820px) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0;
+  }
+`;
+
+const PageNationArea = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 80vh;
+`;
+const Side = styled.div`
+  background-color: gray;
+  align-items: center;
 `;
 
 export default BoardPage;
