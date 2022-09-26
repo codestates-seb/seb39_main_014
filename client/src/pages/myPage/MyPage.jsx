963da01f0dd468c252e0b13e19cf54e08f13fb48
hopak-e <a328028@gmail.com>
2022-09-26 10:12:27 +0900

    Feat:mypage 반응형 구현

--- client/src/pages/myPage/MyPage.jsx
+++ client/src/pages/myPage/MyPage.jsx
@@ -3,10 +3,10 @@ import {
   MypageContainer,
   ContentWrapper,
   Content,
-  UserInfoWarpper,
-  UserInfo,
+  UserInfoWrapper,
+  Profile,
+  UserBoardWrapper,
   UserBoard,
-  BookmarkedBoard,
 } from "./styled";
 import { BsXCircle } from "react-icons/bs";
 
@@ -15,53 +15,55 @@ function MyPage() {
     <MypageContainer>
       <ContentWrapper>
         <Content>
-          <UserInfoWarpper>
-            <div className="Profile">
+          <UserInfoWrapper>
+            <Profile>
               <img alt="profile" src="/assets/logo/only_logo.svg" />
+            </Profile>
+            <label className="Nickname-label">닉네임</label>
+            <input className="Nickname" type="text" defaultValue="아무개" />
+            <div className="Career">
+              <p>웹 프론트엔드 / 초보</p>
             </div>
-            <UserInfo>
-              <div className="Nickname_career">
-                <div className="Nickname">
-                  <div className="Label">닉네임</div>
-                  <input type="text" defaultValue="아몰랑" />
-                </div>
-                <span>웹 프론트엔드 / 초보</span>
-              </div>
-              <div className="Ranking">
-                <div className="Label">활동 점수</div>
-                <div>124 점</div>
-              </div>
-              <div className="Stack">
-                <div className="Label">기술 스택</div>
-                <input type="text" placeholder="기술 스택을 등록하세요!" />
-                <img alt="react" src="/assets/stack/react.svg" />
-                <BsXCircle className="BsXCircle" />
-              </div>
-            </UserInfo>
-          </UserInfoWarpper>
-          <UserBoard>
-            <BookmarkedBoard>
-              <div>
-                <div>북마크한 게시글</div>
+            <label className="Activity-label">활동 점수</label>
+            <div className="Activity">124 점</div>
+            <label className="Stack-label">기술 스택</label>
+            <input
+              className="Registration"
+              type="text"
+              placeholder="기술 스택을 등록하세요!"
+            />
+            <div className="Stack">
+              <img alt="react" src="/assets/stack/react.svg" />
+              <BsXCircle className="BsXCircle" />
+            </div>
+          </UserInfoWrapper>
+          <UserBoardWrapper>
+            <UserBoard>
+              <div className="Myboard">
+                <div className="Bookmark">북마크한 게시글</div>
                 <div>지원한 게시글</div>
               </div>
-              <div>
+              <div className="Checkboard">
                 <input type="checkbox" />
                 <div>스터디 하면서 프로젝트까지 같이 하실분</div>
               </div>
-              <div>
+              <div className="Checkboard">
                 <input type="checkbox" />
                 <div>스터디 하면서 프로젝트까지 같이 하실분</div>
               </div>
-              <div>
+              <div className="Select-all">
                 <div>
                   <input type="checkbox" />
                   <div>전체 선택</div>
                 </div>
                 <button>삭제</button>
               </div>
-            </BookmarkedBoard>
-          </UserBoard>
+              <div className="Modification">
+                <button>완료</button>
+                <button className="Withdrawal">회원 탈퇴</button>
+              </div>
+            </UserBoard>
+          </UserBoardWrapper>
         </Content>
       </ContentWrapper>
     </MypageContainer>
