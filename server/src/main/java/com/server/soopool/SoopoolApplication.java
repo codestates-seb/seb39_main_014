953e9c1ba0a38305f806cf7a55d7be97092e2ebd
euncheol-kim <klmeuncheol@kakao.com>
2022-09-26 16:17:32 +0900

    Feat : Member Entity 임시 데이터 저장

--- server/src/main/java/com/server/soopool/SoopoolApplication.java
+++ server/src/main/java/com/server/soopool/SoopoolApplication.java
@@ -1,18 +1,48 @@
 package com.server.soopool;
 
+import com.server.soopool.member.entity.Member;
+import com.server.soopool.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.security.Principal;
 
 
 // JPA Auditing 활성화
+@Slf4j
 @EnableJpaAuditing
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication()
 public class SoopoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoopoolApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner dataSave(MemberRepository memberRepository) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				Member member = memberRepository.save(
+						Member.builder()
+							.name("홍길동")
+							.userId("hgd2022")
+							.password("123456789")
+							.email("hgd2022@naver.com")
+							.nickname("MrHong")
+							.build()
+				);
+			}
+		};
+	}
 }
