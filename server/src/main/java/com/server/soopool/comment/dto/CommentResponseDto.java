5ebc11e99248eaaf86665e097a001eb7e955b485
euncheol-kim <klmeuncheol@kakao.com>
2022-09-26 16:37:16 +0900

    Feat : comment API Handler과 관련한 RequsetBody dto & ResponseBody dto 작성

---
+++ server/src/main/java/com/server/soopool/comment/dto/CommentResponseDto.java
@@ -0,0 +1,22 @@
+package com.server.soopool.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CommentResponseDto {
+//    private Long boardId;
+    private String nickname;
+    private String content;
+    private Integer groupNumber;
+    private Integer groupSeq;
+    private boolean groupDepth;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifyedAt;
+}
