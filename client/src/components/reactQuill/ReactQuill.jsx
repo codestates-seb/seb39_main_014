362dc3e1943d050ea188168a7bc5963f3f5e0d6d
hopak-e <a328028@gmail.com>
2022-09-27 01:41:31 +0900

    Add: React-quill 에디터 라이브러리 추가

---
+++ client/src/components/reactQuill/ReactQuill.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import styled from "styled-components";
+import "react-quill/dist/quill.snow.css";
+
+const toolbarOptions = [
+  ["link"],
+  [{ header: [1, 2, 3, false] }],
+  ["bold", "italic", "underline", "strike"],
+  ["blockquote"],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ color: [] }, { background: [] }],
+  [{ align: [] }],
+];
+
+export const formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "align",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "background",
+  "color",
+  "link",
+  "image",
+  "video",
+  "width",
+];
+
+const modules = {
+  toolbar: {
+    container: toolbarOptions,
+  },
+};
+
+function Editor() {
+  const [contents, setContents] = useState("");
+
+  return (
+    <QuillContainer>
+      <ReactQuill
+        placeholder={"글을 작성 해주세요."}
+        value={contents}
+        onChange={setContents}
+        theme="snow"
+        modules={modules}
+        formats={formats}
+      ></ReactQuill>
+    </QuillContainer>
+  );
+}
+
+const QuillContainer = styled.div`
+  width: 100%;
+
+  .ql-container {
+    min-height: 200px;
+  }
+`;
+
+export default Editor;
