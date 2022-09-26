84ce298859d42d39d4b4e62e0df1c73f156e083b
so0112 <tkddh0112@gmail.com>
2022-09-26 16:36:50 +0900

    Feat: 회원가입 정합성

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,17 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import InputGroup from "./InputGroup";
+import { FcGoogle } from "react-icons/fc";
 
 // 회원가입 : 이메일, 이름, 닉네임 비밀번호, 비밀번호 확인
 // 로그인 :
+
+/** 이메일 정합성 체크 함수 */
+function checkEmail(email) {
+  let emailReg =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailReg.test(email);
+}
+
+/** 이름 정합성 체크 함수 */
+function checkUsername(username) {
+  let usernameReg = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g;
+  return usernameReg.test(username);
+}
+
+/** 닉네임 정합성 체크 함수 */
+function checkNick(nick) {
+  let nickReg = /^[0-9a-zA-Zㄱ-ㅎ가-힣 ]{2,6}$/;
+  return nickReg.test(nick);
+}
+
+/** 비밀번호 정합성 체크 함수 */
+function checkPassword(password) {
+  let passwordReg = /^[0-9a-zA-Z]{8,16}$/;
+  return passwordReg.test(password);
+}
+
+/** 비밀번호 확인 정합성 체크 함수 */
+function cofirmPassword(password, password2) {
+  return password === password2;
+}
 
 function SignUp() {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [nick, setNick] = useState("");
   const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  // 정합성 검사 state (emailCheck && usernameCheck && nickCheck && passwordCheck && confirmPassword)
+  const [emailCheck, setEmailCheck] = useState(false);
+  const [usernameCheck, setUsernameCheck] = useState(false);
+  const [nickCheck, setNickCheck] = useState(false);
+  const [passwordCheck, setPasswordCheck] = useState(false);
+  const [PasswordConfirm, setPassWordConfirm] = useState(false);
+
+  useEffect(() => {
+    if (checkEmail(email) === false) {
+      setEmailCheck(false);
+    } else if (checkEmail(email) === true) {
+      setEmailCheck(true);
+    }
+  }, [email]);
+
+  useEffect(() => {
+    console.log(checkUsername(username));
+    if (checkUsername(username) === false) {
+      setUsernameCheck(false);
+    } else if (checkEmail(username) === true) {
+      setUsernameCheck(true);
+    }
+  }, [username]);
+
+  useEffect(() => {
+    if (checkNick(nick) === false) {
+      setNickCheck(false);
+    } else if (checkNick(nick) === true) {
+      setNickCheck(true);
+    }
+  }, [nick]);
+
+  useEffect(() => {
+    if (checkPassword(password) === false) {
+      setPasswordCheck(false);
+    } else if (checkPassword(password) === true) {
+      setPasswordCheck(true);
+    }
+  }, [password]);
+
+  useEffect(() => {
+    if (cofirmPassword(password, password2) === false) {
+      setPassWordConfirm(false);
+    } else if (cofirmPassword(password, password2) === true) {
+      setPassWordConfirm(true);
+    }
+  }, [password2]);
 
   //** SignUp 회원가입 POST 버튼 */
   const handleSubmit = async (event) => {
@@ -38,7 +118,10 @@ function SignUp() {
         <form onSubmit={handleSubmit}>
           <h1>회원가입</h1>
           <div className="social-container">
-            <a className="social">구글 아이디로 가입하기</a>
+            <a className="social">
+              <FcGoogle className="goggle-icon" />
+              <p>구글 아이디로 가입하기</p>
+            </a>
           </div>
           <hr />
           <span />
@@ -46,8 +129,13 @@ function SignUp() {
             placeholder="이메일"
             value={email}
             setValue={setEmail}
-            // error={errors.email}
+            // error={error.email}
           />
+          {emailCheck === true ? (
+            <></>
+          ) : (
+            <ContentCheck>올바른 형식의 이메일을 입력해주세요.</ContentCheck>
+          )}
 
           <InputGroup
             placeholder="이름"
@@ -56,20 +144,57 @@ function SignUp() {
             // error={errors.username}
           />
 
+          {usernameCheck === true ? (
+            <></>
+          ) : (
+            <ContentCheck>한글만 입력가능합니다.</ContentCheck>
+          )}
+
           <InputGroup
             placeholder="닉네임"
             value={nick}
             setValue={setNick}
             // error={errors.username}
           />
-
+          {nickCheck === true ? (
+            <></>
+          ) : (
+            <ContentCheck>
+              6글자 이내의 한글, 영어, 숫자 조합만 가능합니다.
+            </ContentCheck>
+          )}
           <InputGroup
             placeholder="비밀번호"
             value={password}
             setValue={setPassword}
+            type="password"
             // error={errors.password}
           />
-          <button type="button">가입하기</button>
+          {passwordCheck === true ? (
+            <></>
+          ) : (
+            <ContentCheck>
+              8~16자 영문 대 소문자, 숫자를 사용하세요.
+            </ContentCheck>
+          )}
+
+          <InputGroup
+            placeholder="비밀번호 확인"
+            type="password"
+            value={password2}
+            setValue={setPassword2}
+            // error={errors.password}
+          />
+
+          {PasswordConfirm === true ? (
+            <></>
+          ) : (
+            <ContentCheck>비밀번호가 일치하지 않습니다.</ContentCheck>
+          )}
+
+          <button type="button" onClick={handleSubmit}>
+            가입하기
+          </button>
           <div className="sign-up">
             <div>이미 아이디가 있으신가요?</div>
             <div className="move-sign-up">
@@ -83,6 +208,9 @@ function SignUp() {
 }
 
 const LoginFrame = styled.div`
+  display: flex;
+  text-align: center;
+  justify-content: center;
   background-color: #fff;
   border-radius: 10px;
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.1), 0 10px 10px rgba(0, 0, 0, 0.22);
@@ -91,7 +219,7 @@ const LoginFrame = styled.div`
   width: 500px;
   max-width: 100%;
   min-height: 550px;
-  height: 600px;
+  height: 800px;
 `;
 
 const FormContainer = styled.div`
@@ -177,6 +305,10 @@ const FormContainer = styled.div`
     margin: 20px 0;
   }
 
+  .goggle-icon {
+    margin-right: 10px;
+    font-size: 25px;
+  }
   .social-container a {
     border: 1px solid #dddddd;
     border-radius: 5px;
@@ -192,6 +324,10 @@ const FormContainer = styled.div`
     cursor: pointer;
   }
 
+  .social-container p {
+    opacity: 0.6;
+  }
+
   .sign-up {
     margin-right: auto;
     margin-top: 15px;
@@ -201,6 +337,13 @@ const FormContainer = styled.div`
   .move-sign-up {
     margin-left: 15px;
   }
+`;
+
+const ContentCheck = styled.p`
+  margin: 0px auto 15px 10px;
+  font-size: 13px;
+  color: red;
+  opacity: 0.8;
 `;
 
 export default SignUp;
