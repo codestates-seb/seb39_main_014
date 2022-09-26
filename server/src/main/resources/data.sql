882e1d1533d0d3cb0644f810b942910e258962e2
yeojoo1224 <yeojoo1224@naver.com>
2022-09-27 04:00:15 +0900

    Add: Career, TechStack 테이블 쿼리 설정

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO career (career_name) VALUES ('백엔드');
+INSERT INTO career (career_name) VALUES ('프론트엔드');
+INSERT INTO career (career_name) VALUES ('웹디자인');
+
+
+
+INSERT INTO tech_stack (tech_stack_name) VALUES ('JAVA');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Spring');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('JavaScript');
