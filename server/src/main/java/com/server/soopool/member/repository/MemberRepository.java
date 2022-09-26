fa662b74cd9da55f6be7e810349be9091c05b0b6
euncheol-kim <klmeuncheol@kakao.com>
2022-09-26 16:22:15 +0900

    Feat : member의 id값을 불러오는 기능 추가 ( findByUserId() )

--- server/src/main/java/com/server/soopool/member/repository/MemberRepository.java
+++ server/src/main/java/com/server/soopool/member/repository/MemberRepository.java
@@ -2,6 +2,11 @@ package com.server.soopool.member.repository;
 
 import com.server.soopool.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUserId(String memberId);
 }
