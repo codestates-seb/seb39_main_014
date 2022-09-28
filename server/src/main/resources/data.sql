75af8f36a19c49ff37f8c163eccff554db7a2248
yeojoo1224 <yeojoo1224@naver.com>
2022-09-28 22:18:22 +0900

    Modify: Career, TechStack 테이블 row 추가

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,9 +1,35 @@
-INSERT INTO career (career_name) VALUES ('백엔드');
-INSERT INTO career (career_name) VALUES ('프론트엔드');
-INSERT INTO career (career_name) VALUES ('웹디자인');
+INSERT INTO career (career_name) VALUES ('웹 프론트엔드');
+INSERT INTO career (career_name) VALUES ('웹 백엔드');
+INSERT INTO career (career_name) VALUES ('웹 디자이너');
+INSERT INTO career (career_name) VALUES ('모바일');
+INSERT INTO career (career_name) VALUES ('기타');
 
 
-
-INSERT INTO tech_stack (tech_stack_name) VALUES ('JAVA');
-INSERT INTO tech_stack (tech_stack_name) VALUES ('Spring');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Java');
 INSERT INTO tech_stack (tech_stack_name) VALUES ('JavaScript');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('React');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Node');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Next');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Express');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Vue');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('TypeScript');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('AWS');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Docker');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Unity');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Swift');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Svelte');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Spring');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('ReactNative');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Python');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Php');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Nest');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('MySQL');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('MongoDB');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Kotlin');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('GraphQl');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Go');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Flutter');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Firebase');
+INSERT INTO tech_stack (tech_stack_name) VALUES ('Figma');
+
+
