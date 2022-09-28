526f13d3698fc2099cefc147f07faf9cee28af61
so0112 <tkddh0112@gmail.com>
2022-09-28 17:04:31 +0900

    Design: 반응형 Nav

--- client/src/components/navigation/Nav.jsx
+++ client/src/components/navigation/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import styled from "styled-components";
 
@@ -7,35 +8,38 @@ function Nav() {
   return (
     <NavFrame>
       <NavContainer>
-        <LeftMenu>
-          <Logo>
+        <Logo>
+          <div>
+            <StyledLink to="/">
+              <img
+                src="/assets/logo/logo_black.png"
+                alt="asd.png"
+                width={100}
+                height={50}
+              />
+            </StyledLink>
+          </div>
+        </Logo>
+        <Menu>
+          <div className="left-menu">
             <div>
-              <Link to="/">
-                <img
-                  src="/assets/logo/logo_black.png"
-                  alt="asd.png"
-                  width={100}
-                  height={50}
-                />
-              </Link>
+              <StyledLink to="/board">전체</StyledLink>
             </div>
-          </Logo>
-          <Menu>
-            <Link to="/board" style={{ textDecoration: "none" }}>
-              <div>전체</div>
-            </Link>
             <div>스터디</div>
             <div>프로젝트</div>
-          </Menu>
-        </LeftMenu>
-        <RightMenu>
-          <Link to="/board/write" style={{ textDecoration: "none" }}>
-            <div>작성하기</div>
-          </Link>
-          <Link to="/login" style={{ textDecoration: "none" }}>
-            <div>로그인</div>
-          </Link>
-        </RightMenu>
+          </div>
+
+          <div className="right-menu">
+            <StyledLink to="/board/write">
+              <div>작성하기</div>
+            </StyledLink>
+            <StyledLink to="/login">
+              <div>로그인</div>
+            </StyledLink>
+          </div>
+
+          <GiHamburgerMenu className="hambuger" />
+        </Menu>
       </NavContainer>
     </NavFrame>
   );
@@ -44,65 +48,80 @@ function Nav() {
 const NavFrame = styled.nav`
   background: white;
   height: 5rem;
+
   display: flex;
-  font-size: 1rem;
-  position: sticky;
+  align-items: center;
   justify-content: center;
+
+  position: sticky;
+
   top: 0;
   z-index: 10;
-  /* margin-top: 0.5rem;
-  margin-bottom: 0.5rem; */
-
-  div {
-    color: 5f5f5f;
-  }
 
   font-size: 20px;
   font-weight: bold;
   color: #5f5f5f;
-  @media screen and (max-width: 1280px) {
-    transition: 0.8s all ease;
-  }
 `;
 
 const NavContainer = styled.div`
-  width: 1280px;
-  height: 100%;
+  width: 1000px;
   padding: 2rem;
   display: flex;
-  justify-content: space-between;
+  align-items: center;
   @media screen and (max-width: 768px) {
-    padding: 0;
   }
 `;
 
-const LeftMenu = styled.div`
-  display: flex;
-`;
-
 const Logo = styled.div`
-  display: flex;
-  align-items: center;
   cursor: pointer;
 `;
 
 const Menu = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  width: 80%;
+  justify-content: space-between;
+
+  .left-menu {
+    display: flex;
+    margin-left: 10px;
+  }
+
+  .right-menu {
+    display: flex;
+    margin-right: 10px;
+  }
+
   div {
     margin-left: 40px;
-    color: #5f5f5f;
+  }
+
+  .hambuger {
+    display: none;
+    margin-left: auto;
+    margin-right: 20px;
+  }
+
+  @media screen and (max-width: 768px) {
+    align-items: center;
+
+    .left-menu {
+      display: none;
+      margin-left: 10px;
+    }
+
+    .right-menu {
+      display: none;
+    }
+
+    .hambuger {
+      display: block;
+    }
   }
 `;
 
-const RightMenu = styled.div`
-  display: flex;
-  align-items: center;
-  div {
-    color: #5f5f5f;
-    margin-right: 50px;
-  }
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #5f5f5f;
 `;
 
 export default Nav;
