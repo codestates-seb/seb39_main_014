fb58a09630d744a8a537ca471225bbe8b1602a51
yeojoo1224 <yeojoo1224@naver.com>
2022-09-28 22:24:01 +0900

    Modify: findByBoardId 추가

--- server/src/main/java/com/server/soopool/boardTechstack/repository/BoardTechStackRepository.java
+++ server/src/main/java/com/server/soopool/boardTechstack/repository/BoardTechStackRepository.java
@@ -3,5 +3,10 @@ package com.server.soopool.boardTechstack.repository;
 import com.server.soopool.boardTechstack.entity.BoardTechStack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BoardTechStackRepository extends JpaRepository<BoardTechStack, Long> {
+
+    Optional<BoardTechStack> findByBoardId(Long boardId);
 }
