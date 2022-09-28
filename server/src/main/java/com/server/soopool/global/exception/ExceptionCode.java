c5d4fac8dbbd15714df36c701a832438e56dd707
yeojoo1224 <yeojoo1224@naver.com>
2022-09-28 22:19:06 +0900

    Modify: Career, TechStack 관련 exception 추가

--- server/src/main/java/com/server/soopool/global/exception/ExceptionCode.java
+++ server/src/main/java/com/server/soopool/global/exception/ExceptionCode.java
@@ -21,7 +21,11 @@ public enum ExceptionCode {
     //BOARD EXCEPTION
 
     BOARD_NOT_FOUND(404,"게시글을 찾을 수 없습니다."),
-    COMMENT_NOT_FOUND(404,"댓글을 찾을 수 없습니다.");
+    COMMENT_NOT_FOUND(404,"댓글을 찾을 수 없습니다."),
+
+    CAREER_NOT_FOUND(404, "커리어를 찾을 수 없습니다."),
+    TECHSTACK_NOT_FOUND(404, "사용기술을 찾을 수 없습니다.");
+
 
     @Getter
     private int status;
