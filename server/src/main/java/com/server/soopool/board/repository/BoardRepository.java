3ef25f59fed8e4f11f6709d2d9509be3401c47f9
euncheol-kim <klmeuncheol@kakao.com>
2022-09-28 14:59:51 +0900

    Modify : 현재 프로젝트에서 사용중이지 않은 부분 주석처리(추후, 필요유무 판단에 따른 작성이 필요함)

--- server/src/main/java/com/server/soopool/board/repository/BoardRepository.java
+++ server/src/main/java/com/server/soopool/board/repository/BoardRepository.java
@@ -2,10 +2,7 @@ package com.server.soopool.board.repository;
 
 import com.server.soopool.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,11 +15,12 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
            board table의 comment_amount의 값을 설정하기 위한 메소드를 작성하려고 했습니다.
            필요 없으시다고 판단이 들면 지우시면 됩니다.
+
+        @Modifying(clearAutomatically = true)
+        @Transactional
+        @Query(value = "UPDATE Board b " +
+                "SET b.commentAmount = :commentAmount " +
+                "WHERE b.memberId = :memberId")
+        public void modifyCommentCountColumn();
     */
-    @Modifying(clearAutomatically = true)
-    @Transactional
-    @Query(value = "UPDATE Board b " +
-            "SET b.commentAmount = :commentAmount " +
-            "WHERE b.memberId = :memberId")
-    public void modifyCommentCountColumn();
 }
