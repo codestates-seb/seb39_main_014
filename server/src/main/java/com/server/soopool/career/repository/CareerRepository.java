8a82467aa01939652003a142abdc015a38313d52
yeojoo1224 <yeojoo1224@naver.com>
2022-09-28 22:24:39 +0900

    Modify: findById 추가

--- server/src/main/java/com/server/soopool/career/repository/CareerRepository.java
+++ server/src/main/java/com/server/soopool/career/repository/CareerRepository.java
@@ -3,5 +3,9 @@ package com.server.soopool.career.repository;
 import com.server.soopool.career.entity.Career;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CareerRepository extends JpaRepository<Career, Long> {
+
+    Optional<Career> findById(Long id);
 }
