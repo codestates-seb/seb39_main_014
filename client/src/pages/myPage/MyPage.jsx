1675c66392fc6b66625f7eb3e497277aac7af04e
hopak-e <a328028@gmail.com>
2022-09-29 23:33:51 +0900

    Feat:상태 추가 및 onClick 이벤트 구현

--- client/src/pages/myPage/MyPage.jsx
+++ client/src/pages/myPage/MyPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import {
   MypageContainer,
   ContentWrapper,
@@ -8,10 +8,39 @@ import {
   UserBoardWrapper,
   UserBoard,
 } from "./styled";
-import { BsXCircle } from "react-icons/bs";
+import {
+  careerLists,
+  levelLists,
+  stackLists,
+} from "../../pages/writeForm/WriteFormData";
+import { GoX } from "react-icons/go";
+import { AiOutlineDown } from "react-icons/ai";
 
 function MyPage() {
-  const [career, setCareer] = useState({ job: "", proficiency: "" });
+  const [career, setCareer] = useState({
+    name: "웹 프론트엔드",
+    level: "초보",
+  });
+  const [isCareer, setIsCareer] = useState(false);
+  const outSection = useRef();
+
+  const [nickname, setNickname] = useState("아무개");
+
+  const [activeScore, setActiveScore] = useState(0);
+
+  const [techStack, setTechStack] = useState([]);
+  const [newStackList, setNewStackList] = useState(stackLists);
+  const [istechStackList, setIsTechStackList] = useState(false);
+  const stackRef = useRef(0);
+  const newStackRef = useRef(27);
+
+  const [search, setSearch] = useState("");
+
+  const searchStack = newStackList.filter((prev) => {
+    if (search === "") {
+      return prev;
+    } else return prev.stack.toLowerCase().includes(search.toLowerCase());
+  });
 
   return (
     <MypageContainer>
@@ -22,21 +51,128 @@ function MyPage() {
               <img alt="profile" src="/assets/logo/only_logo.svg" />
             </Profile>
             <label className="Nickname-label">닉네임</label>
-            <input className="Nickname" type="text" defaultValue="아무개" />
-            <div className="Career">
-              <p>웹 프론트엔드 / 초보</p>
+            <input
+              className="Nickname"
+              type="text"
+              defaultValue={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
+            <div
+              className="Career"
+              ref={outSection}
+              onClick={(e) => {
+                if (outSection.current === e.target) {
+                  setIsCareer(!career);
+                }
+              }}
+            >
+              <p onClick={() => setIsCareer(!isCareer)}>
+                {career.name} / {career.level}
+              </p>
+              {isCareer ? (
+                <div className="Career-level-lists">
+                  <ul className="Career-list">
+                    {careerLists.map((el) => (
+                      <li
+                        key={el.id}
+                        onClick={() =>
+                          setCareer((prev) => {
+                            return { ...prev, name: el.career };
+                          })
+                        }
+                      >
+                        {el.career}
+                      </li>
+                    ))}
+                  </ul>
+                  <ul className="Level-list">
+                    {levelLists.map((el) => (
+                      <li
+                        key={el.id}
+                        onClick={() =>
+                          setCareer((prev) => {
+                            return { ...prev, level: el.level };
+                          })
+                        }
+                      >
+                        {el.level}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ) : null}
             </div>
             <label className="Activity-label">활동 점수</label>
-            <div className="Activity">124 점</div>
+            <div className="Activity">{activeScore}점</div>
             <label className="Stack-label">기술 스택</label>
-            <input
-              className="Registration"
-              type="text"
-              placeholder="기술 스택을 등록하세요!"
-            />
+            <div
+              className="Registration-box"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsTechStackList(!istechStackList);
+              }}
+            >
+              <input
+                className="Registration"
+                type="text"
+                placeholder="기술 스택을 등록하세요!"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <AiOutlineDown
+                className="AiOutlineDown"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsTechStackList(!istechStackList);
+                }}
+              />{" "}
+              {istechStackList ? (
+                <ul className="Stacklist">
+                  {searchStack.map((el) => (
+                    <li
+                      key={el.id}
+                      onClick={(e) => {
+                        setTechStack([
+                          ...techStack,
+                          { name: el.stack, id: stackRef.current },
+                        ]);
+                        stackRef.current = stackRef.current + 1;
+                        setNewStackList(
+                          newStackList.filter((prev) => prev.stack !== el.stack)
+                        );
+                      }}
+                    >
+                      {el.stack}
+                    </li>
+                  ))}
+                </ul>
+              ) : null}
+            </div>
+
             <div className="Stack">
-              <img alt="react" src="/assets/stack/react.svg" />
-              <BsXCircle className="BsXCircle" />
+              {techStack
+                ? techStack.map((el) => (
+                    <div key={el.id}>
+                      <img alt={el.name} src={`/assets/stack/${el.name}.svg`} />
+                      <GoX
+                        className="Gox"
+                        onClick={() => {
+                          setTechStack(
+                            techStack.filter((prev) => prev.id !== el.id)
+                          );
+                          setNewStackList([
+                            ...newStackList,
+                            {
+                              id: newStackRef.current,
+                              stack: el.name,
+                            },
+                          ]);
+                          newStackRef.current = newStackRef.current + 1;
+                        }}
+                      />
+                    </div>
+                  ))
+                : null}
             </div>
           </UserInfoWrapper>
           <UserBoardWrapper>
