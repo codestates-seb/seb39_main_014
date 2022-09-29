11d3d46ce7dab25d91cc5e51b35d4a5a75d1fe6f
euncheol-kim <klmeuncheol@kakao.com>
2022-09-29 18:37:32 +0900

    Modify : 주석 추가 및 삭제

--- server/src/main/java/com/server/soopool/member/controller/MemberController.java
+++ server/src/main/java/com/server/soopool/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.server.soopool.member.controller;
 
 import com.server.soopool.auth.PrincipalDetails;
+import com.server.soopool.bookmark.dto.BookmarkDeleteDto;
 import com.server.soopool.bookmark.response.BookmarkListResponse;
 import com.server.soopool.bookmark.entity.Bookmark;
 import com.server.soopool.bookmark.mapper.BookmarkMapper;
@@ -11,10 +12,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +25,7 @@ public class MemberController {
     private final BookmarkService bookmarkService;
     private final BookmarkMapper bookmarkMapper;
 
+    // 마이페이지 북마크 조회
     @GetMapping("my-page/bookmark")
     public ResponseEntity getUserInfoBookmark(@AuthenticationPrincipal PrincipalDetails principal) {
         Member member = memberService.findByUserId("hgd2022");
@@ -37,14 +36,40 @@ public class MemberController {
                         bookmarkMapper.bookmarkToBookmarkOfUserResponseDto(bookmark)
                 ),
                 HttpStatus.OK
-            );
+        );
     }
 
+    // 마이페이지 특정 북마크 삭제
+    //todo : RequestDto 해결 및 서비스로직구현
     @DeleteMapping("my-page/bookmark")
-    public ResponseEntity deleteUserInfoBookmark(@AuthenticationPrincipal PrincipalDetails principal){
+    public ResponseEntity deleteUserInfoBookmark(@AuthenticationPrincipal PrincipalDetails principal,
+                                                 @RequestBody BookmarkDeleteDto bookmarkDeleteDto) {
         Member member = memberService.findByUserId("hgd2022");
-//        List<Bookmark> bookmark = bookmarkService.deleteBookmark(member);
+        bookmarkService.deleteBookmark(member, bookmarkDeleteDto.getBookmarkList());
+        List<Bookmark> bookmark = bookmarkService.getUserBookmark(member);
 
-        return null;
+        return new ResponseEntity<>(
+                new BookmarkListResponse<>(
+                        bookmarkMapper.bookmarkToBookmarkOfUserResponseDto(bookmark)
+                ),
+                HttpStatus.OK
+        );
     }
+
+
+
+    //     마이페이지 북마크 전체 삭제
+//    @DeleteMapping("my-page/bookmark")
+//    public ResponseEntity deleteAllUserInfoBookmark(@AuthenticationPrincipal PrincipalDetails principal) {
+//        Member member = memberService.findByUserId("hgd2022");
+//        bookmarkService.deleteAllBookmark(member);
+//        List<Bookmark> bookmark = bookmarkService.getUserBookmark(member);
+//
+//        return new ResponseEntity<>(
+//                new BookmarkListResponse<>(
+//                        bookmarkMapper.bookmarkToBookmarkOfUserResponseDto(bookmark)
+//                ),
+//                HttpStatus.OK
+//        );
+//    }
 }
