07968691ba72d80f71c48005c336b1c2804b352f
so0112 <tkddh0112@gmail.com>
2022-09-30 21:04:17 +0900

    Modify: 온라인 지역무관으로 변경

---
+++ client/src/components/topButton/TopButton.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+function TopButton() {
+  return (
+    <ScrollContainer>
+      <BsFillArrowUpCircleFill id="top" onClick={scrollToTop} type="button" />
+    </ScrollContainer>
+  );
+}
+
+const ScrollContainer = styled.div`
+  z-index: 1;
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 100px;
+
+  #top {
+    width: 50px;
+    height: 50px;
+
+    font-weight: bold;
+    font-size: 15px;
+    color: green;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  #top:hover {
+    color: green;
+  }
+`;
+
+export default TopButton;
