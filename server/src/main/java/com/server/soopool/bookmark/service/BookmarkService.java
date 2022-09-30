7412671d48e46a55da0a0b34161fa25642f25de6
yeojoo1224 <yeojoo1224@naver.com>
2022-09-30 17:57:57 +0900

    Modify: BookmarkService 변경

--- server/src/main/java/com/server/soopool/bookmark/service/BookmarkService.java
+++ server/src/main/java/com/server/soopool/bookmark/service/BookmarkService.java
@@ -4,12 +4,14 @@ import com.server.soopool.board.entity.Board;
 import com.server.soopool.board.repository.BoardRepository;
 import com.server.soopool.bookmark.entity.Bookmark;
 import com.server.soopool.bookmark.repository.BookMarkRepository;
+import com.server.soopool.bookmark.request.BookmarkRequest;
 import com.server.soopool.member.entity.Member;
+import com.server.soopool.member.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @AllArgsConstructor
@@ -17,7 +19,22 @@ import java.util.Optional;
 public class BookmarkService {
 
     private final BookMarkRepository bookMarkRepository;
+    private final MemberRepository memberRepository;
+
     private final BoardRepository boardRepository;
+
+    public List<Bookmark> getUserBookmark(Member member) {
+        Member searchMembers = memberRepository.findById(member.getId()).get();
+        List<Bookmark> bookmarks = bookMarkRepository.findByBookmark(searchMembers);
+
+        return bookmarks;
+    }
+
+    public void deleteBookmark(Member member, List<BookmarkRequest> bookmarkRequests) {
+        for(BookmarkRequest e : bookmarkRequests) {
+            bookMarkRepository.deleteByColumns(member, e.getBoardId());
+        }
+    }
 
     public void createBookmark(Member member, Board board) {
         bookMarkRepository.saveByColumns(member,board);
