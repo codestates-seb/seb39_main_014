46a185340e691e7baf04ad2abf66e32b685cc2cb
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:25:41 +0900

    Setter 추가

--- server/src/main/java/com/server/soopool/career/request/UserInfoCareerRequest.java
+++ server/src/main/java/com/server/soopool/career/request/UserInfoCareerRequest.java
@@ -1,8 +1,10 @@
 package com.server.soopool.career.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserInfoCareerRequest {
     private String name;
     private String level;
