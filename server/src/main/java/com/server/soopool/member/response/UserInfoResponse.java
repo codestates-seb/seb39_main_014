3fc01937f53326b5fc8468714ce3053cf9a99aed
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:43:56 +0900

    UserInfoResponseDto 추가

---
+++ server/src/main/java/com/server/soopool/member/response/UserInfoResponse.java
@@ -0,0 +1,14 @@
+package com.server.soopool.member.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserInfoResponse<T> {
+    private List<T> userInfo;
+}
