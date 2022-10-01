4f6d0d110f397d715ce40bac9a686fde3dc56435
so0112 <tkddh0112@gmail.com>
2022-10-01 22:25:38 +0900

    Feat: API 파일

---
+++ client/src/api/getBoard.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export default function getBoard(BOARD_URL, setState) {
+  axios
+    .get(BOARD_URL, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+    .then((res) => {
+      setState(res.data.boards);
+    });
+}
