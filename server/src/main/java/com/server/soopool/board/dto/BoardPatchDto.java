be2bff5aef40ebb6174239174e1028ff31063ecb
yeojoo1224 <yeojoo1224@naver.com>
2022-10-01 18:01:38 +0900

    Modify: BoardPatchDto 수정

--- server/src/main/java/com/server/soopool/board/dto/BoardPatchDto.java
+++ server/src/main/java/com/server/soopool/board/dto/BoardPatchDto.java
@@ -1,35 +1,32 @@
 package com.server.soopool.board.dto;
 
-import com.server.soopool.board.entity.Board;
 import com.server.soopool.boardCareer.dto.BoardCareerDto;
-import com.server.soopool.boardCareer.entity.BoardCareer;
 import com.server.soopool.boardTechstack.dto.BoardTechStackDto;
-import com.server.soopool.boardTechstack.entity.BoardTechStack;
 
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 import java.util.List;
-import java.util.Optional;
 
 
 @Getter
 public class BoardPatchDto {
 
-    private long boardId;
+    @NotBlank(message = "모집 구분을 선택해주세요.")
+    private String recruitCategory;
 
     @NotBlank(message = "온라인 오프라인 여부를 선택해주세요.")
-    private Board.RecruitMethod recruitMethod;
+    private String recruitMethod;
 
     @NotBlank(message = "지역을 선택해주세요.")
-    private Board.Location location;
+    private String location;
 
     @NotBlank(message = "사용 기술을 선택해주세요.")
     private List<BoardTechStackDto> boardTechStacks;
 
-    private Board.Period period;
+    private String period;
 
-    @NotBlank(message = "모집 인원수를 선택해주세요.")
+    @NotBlank(message = "모집 커리어와 모집인원을 선택해주세요")
     private List<BoardCareerDto> boardCareers;
 
     private String contact;
@@ -39,9 +36,5 @@ public class BoardPatchDto {
 
     @NotBlank(message = "내용을 입력해주세요.")
     private String contents;
-
-    public void setBoardId(long boardId) {
-        this.boardId = boardId;
-    }
 
 }
