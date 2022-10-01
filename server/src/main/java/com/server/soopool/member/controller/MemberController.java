b7e7b896a17e007965bcbd34ca9e1f31a9698268
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:25:09 +0900

    MyPage Post, Get구현

--- server/src/main/java/com/server/soopool/member/controller/MemberController.java
+++ server/src/main/java/com/server/soopool/member/controller/MemberController.java
@@ -1,26 +1,33 @@
 package com.server.soopool.member.controller;
 
+import com.nimbusds.openid.connect.sdk.claims.UserInfo;
 import com.server.soopool.auth.PrincipalDetails;
 import com.server.soopool.bookmark.dto.BookmarkDeleteDto;
 import com.server.soopool.bookmark.response.BookmarkListResponse;
 import com.server.soopool.bookmark.entity.Bookmark;
 import com.server.soopool.bookmark.mapper.BookmarkMapper;
 import com.server.soopool.bookmark.service.BookmarkService;
+import com.server.soopool.career.request.UserInfoCareerRequest;
 import com.server.soopool.careerMember.entity.CareerMember;
 import com.server.soopool.careerMember.service.CareerMemberService;
 import com.server.soopool.member.dto.UserInfoDto;
 import com.server.soopool.member.entity.Member;
 import com.server.soopool.member.response.UserInfoResponse;
+import com.server.soopool.member.response.UserInfoResponseDto;
 import com.server.soopool.member.service.MemberService;
 import com.server.soopool.memberTechstack.entity.MemberTechStack;
 import com.server.soopool.memberTechstack.service.MemberTechstackService;
 import com.server.soopool.techstack.entity.TechStack;
+import com.server.soopool.techstack.request.UserInfoTechStackRequest;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -70,10 +77,37 @@ public class MemberController {
     // 회원정보 info 가져오기
     @GetMapping("my-page/info")
     public ResponseEntity getUserInfo(@AuthenticationPrincipal PrincipalDetails principal) {
-        return null;
+
+        Member member = memberService.findByUserId("hgd2022");
+        List<UserInfoCareerRequest> userInfoCareerRequests = new ArrayList<>();
+        UserInfoCareerRequest userInfoCareerRequest = new UserInfoCareerRequest();
+        if(member.getCareerMembers().size() != 0) {
+            userInfoCareerRequest.setName(member.getCareerMembers().get(0).getCareerId().getCareerName());
+        }
+        if(member.getCareerMembers().size() != 0) {
+            userInfoCareerRequest.setLevel(member.getCareerMembers().get(0).getCareerLevelName().getName());
+        }
+        userInfoCareerRequests.add(userInfoCareerRequest);
+
+        List<UserInfoTechStackRequest> userInfoTechStackRequests = new ArrayList<>();
+        for(MemberTechStack memberTechStack1 : member.getMemberTechStacks()) {
+            UserInfoTechStackRequest userInfoTechStackRequest1 = new UserInfoTechStackRequest();
+            userInfoTechStackRequest1.setName(memberTechStack1.getTechStackId().getTechStackName());
+            userInfoTechStackRequest1.setImageUri(memberTechStack1.getTechStackId().getImageUri());
+            userInfoTechStackRequests.add(userInfoTechStackRequest1);
+        }
+
+        UserInfoResponseDto userInfoResponseDto = new UserInfoResponseDto();
+        userInfoResponseDto.setNickname(member.getNickname());
+        userInfoResponseDto.setActiveScore(0);
+        userInfoResponseDto.setCareer(userInfoCareerRequests);
+        userInfoResponseDto.setTechStack(userInfoTechStackRequests);
+
+        return new ResponseEntity<>(userInfoResponseDto,HttpStatus.OK);
     }
 
     @PostMapping("my-page/info")
+    @Transactional
     public ResponseEntity postUserInfo(@AuthenticationPrincipal PrincipalDetails principal,
                                        @RequestBody UserInfoDto userInfoDto) {
         //Member nickname 변경
@@ -88,9 +122,31 @@ public class MemberController {
         List<MemberTechStack> memberTechStack = memberTechStackService.getMemberTechStack(member);
         memberTechStackService.setMemberTechStack(member, memberTechStack,userInfoDto.getTechStack());
 
-        return new ResponseEntity<>(
-//                new UserInfoResponse(),
-                HttpStatus.OK);
+        //
+        List<UserInfoCareerRequest> userInfoCareerRequests = new ArrayList<>();
+        UserInfoCareerRequest userInfoCareerRequest = new UserInfoCareerRequest();
+        if(member.getCareerMembers().size() != 0) {
+            userInfoCareerRequest.setName(member.getCareerMembers().get(0).getCareerId().getCareerName());
+        }
+        if(member.getCareerMembers().size() != 0) {
+            userInfoCareerRequest.setLevel(member.getCareerMembers().get(0).getCareerLevelName().getName());
+        }
+        userInfoCareerRequests.add(userInfoCareerRequest);
+
+        List<UserInfoTechStackRequest> userInfoTechStackRequests = new ArrayList<>();
+        for(MemberTechStack memberTechStack1 : member.getMemberTechStacks()) {
+            UserInfoTechStackRequest userInfoTechStackRequest1 = new UserInfoTechStackRequest();
+            userInfoTechStackRequest1.setName(memberTechStack1.getTechStackId().getTechStackName());
+            userInfoTechStackRequests.add(userInfoTechStackRequest1);
+        }
+
+        UserInfoResponseDto userInfoResponseDto = new UserInfoResponseDto();
+        userInfoResponseDto.setNickname(member.getNickname());
+        userInfoResponseDto.setActiveScore(0);
+        userInfoResponseDto.setCareer(userInfoCareerRequests);
+        userInfoResponseDto.setTechStack(userInfoTechStackRequests);
+
+        return new ResponseEntity<>(userInfoResponseDto,HttpStatus.OK);
     }
 }
 
