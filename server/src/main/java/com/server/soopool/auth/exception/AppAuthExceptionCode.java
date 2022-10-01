a716a2e105ce2161c9942d2e811b7d45ddecfeaa
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-01 18:14:30 +0900

    Modify : Getter 위치 변경 및 ErrorStatus 추가 및 Error Message 한글화

--- server/src/main/java/com/server/soopool/auth/exception/AppAuthExceptionCode.java
+++ server/src/main/java/com/server/soopool/auth/exception/AppAuthExceptionCode.java
@@ -2,25 +2,30 @@ package com.server.soopool.auth.exception;
 
 import lombok.Getter;
 
-@Getter
+
 public enum AppAuthExceptionCode {
 
-    ACCESS_TOKEN_EXPIRED("Access token expired"),
-    INVALID_ACCESS_TOKEN("Invalid access token"),
-    ACCESS_TOKEN_NOT_EXIST("Access token not exist"),
-    REFRESH_TOKEN_EXPIRED("Refresh token expired"),
-    INVALID_REFRESH_TOKEN("Refresh token is empty"),
-    REFRESH_TOKEN_NOT_EXIST("Refresh token not exist"),
-    INVALID_EMAIL_OR_PASSWORD("Invalid email or password"),
-    DATA_DESERIALIZE_ERROR("Failed to login request body deserialization"),
-    EXISTS_MEMBER("Member already exists"),
-    OAUTH2_AUTH_FAILURE("OAuth2 Authentication failure"),
-    INVALID_OAUTH2_PROVIDER("Invalid OAuth2 provider"),
-    UNDEFINED("Undefined");
+    ACCESS_TOKEN_EXPIRED(401,"액세스 토큰이 만료되었습니다."),
+    INVALID_ACCESS_TOKEN(401,"유효하지 않는 액세스 토큰 입니다."),
+    ACCESS_TOKEN_NOT_EXIST(401,"액세스 토큰이 존재하지 않습니다."),
+    REFRESH_TOKEN_EXPIRED(401,"리프레시 토큰이 만료되었습니다."),
+    INVALID_REFRESH_TOKEN(401,"리프레시 토큰이 비어있습니다."),
+    REFRESH_TOKEN_NOT_EXIST(401,"리프레시 토큰이 존재하지 않습니다."),
+    INVALID_ID_OR_PASSWORD(401,"유효하지 않은 아이디 혹은 비밀번호 입니다."),
+    DATA_DESERIALIZE_ERROR(500,"Failed to login request body deserialization"),
+    EXISTS_MEMBER(401,"이미 존재하는 아이디 입니다."),
+    OAUTH2_AUTH_FAILURE(401,"OAuth2 인증에 실패했습니다."),
+    INVALID_OAUTH2_PROVIDER(401,"유효하지 않은 OAuth2 Provider입니다."),
+    UNDEFINED(500,"알 수 없는 오류가 발생했습니다.");
 
+    @Getter
+    private final int status;
+
+    @Getter
     private final String message;
 
-    AppAuthExceptionCode(String message){
+    AppAuthExceptionCode(int status, String message){
+        this.status = status;
         this.message = message;
     }
 
