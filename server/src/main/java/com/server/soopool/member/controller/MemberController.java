6938482b4ea8934b64bcd79062ce482391828e77
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:37:40 +0900

    마이페이지 정보 Post, Get 구현

--- server/src/main/java/com/server/soopool/member/controller/MemberController.java
+++ server/src/main/java/com/server/soopool/member/controller/MemberController.java
@@ -6,8 +6,15 @@ import com.server.soopool.bookmark.response.BookmarkListResponse;
 import com.server.soopool.bookmark.entity.Bookmark;
 import com.server.soopool.bookmark.mapper.BookmarkMapper;
 import com.server.soopool.bookmark.service.BookmarkService;
+import com.server.soopool.careerMember.entity.CareerMember;
+import com.server.soopool.careerMember.service.CareerMemberService;
+import com.server.soopool.member.dto.UserInfoDto;
 import com.server.soopool.member.entity.Member;
+import com.server.soopool.member.response.UserInfoResponse;
 import com.server.soopool.member.service.MemberService;
+import com.server.soopool.memberTechstack.entity.MemberTechStack;
+import com.server.soopool.memberTechstack.service.MemberTechstackService;
+import com.server.soopool.techstack.entity.TechStack;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +22,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -24,6 +32,9 @@ public class MemberController {
     private final MemberService memberService;
     private final BookmarkService bookmarkService;
     private final BookmarkMapper bookmarkMapper;
+    private final CareerMemberService careerMemberService;
+
+    private final MemberTechstackService memberTechStackService;
 
     // 마이페이지 북마크 조회
     @GetMapping("my-page/bookmark")
@@ -56,20 +67,51 @@ public class MemberController {
         );
     }
 
+    // 회원정보 info 가져오기
+    @GetMapping("my-page/info")
+    public ResponseEntity getUserInfo(@AuthenticationPrincipal PrincipalDetails principal) {
+        return null;
+    }
 
+    @PostMapping("my-page/info")
+    public ResponseEntity postUserInfo(@AuthenticationPrincipal PrincipalDetails principal,
+                                       @RequestBody UserInfoDto userInfoDto) {
+        //Member nickname 변경
+        Member member = memberService.findByUserId("hgd2022");
+        memberService.setMemberNickname(member, userInfoDto.getNickname());
 
-    //     마이페이지 북마크 전체 삭제
-//    @DeleteMapping("my-page/bookmark")
-//    public ResponseEntity deleteAllUserInfoBookmark(@AuthenticationPrincipal PrincipalDetails principal) {
-//        Member member = memberService.findByUserId("hgd2022");
-//        bookmarkService.deleteAllBookmark(member);
-//        List<Bookmark> bookmark = bookmarkService.getUserBookmark(member);
-//
-//        return new ResponseEntity<>(
-//                new BookmarkListResponse<>(
-//                        bookmarkMapper.bookmarkToBookmarkOfUserResponseDto(bookmark)
-//                ),
-//                HttpStatus.OK
-//        );
-//    }
+        // Member career 추가 및 변경
+        Optional<CareerMember> careerMember = careerMemberService.getCareerMember(member);
+        careerMemberService.setCareerMember(member, careerMember, userInfoDto.getCareer());
+
+        // TechStack 추가
+        Optional<MemberTechStack> memberTechStack = memberTechStackService.getMemberTechStack(member);
+        memberTechStackService.setMemberTechStack(member, memberTechStack,userInfoDto.getTechStack());
+
+        return new ResponseEntity<>(
+//                new UserInfoResponse(),
+                HttpStatus.OK);
+    }
 }
+
+
+/*
+    마이페이지 북마크 전체 삭제 (작성자 김은철)
+     - 사용되어질 수 있어서 빼놨습니다.
+     - 지우지 말아주세요.
+
+        @DeleteMapping("my-page/bookmark")
+        public ResponseEntity deleteAllUserInfoBookmark(@AuthenticationPrincipal PrincipalDetails principal) {
+            Member member = memberService.findByUserId("hgd2022");
+            bookmarkService.deleteAllBookmark(member);
+            List<Bookmark> bookmark = bookmarkService.getUserBookmark(member);
+
+            return new ResponseEntity<>(
+                    new BookmarkListResponse<>(
+                            bookmarkMapper.bookmarkToBookmarkOfUserResponseDto(bookmark)
+                    ),
+                    HttpStatus.OK
+            );
+        }
+
+*/
