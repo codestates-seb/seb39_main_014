eb6c95bcc5891db20891f9fb62e935665fa20fa1
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:41:53 +0900

    패키지 변경

--- server/src/main/java/com/server/soopool/SoopoolApplication.java
+++ server/src/main/java/com/server/soopool/SoopoolApplication.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
