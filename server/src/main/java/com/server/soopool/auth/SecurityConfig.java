e520e115e2223417225e6c3a13fc3cf56d1173b1
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:41:30 +0900

    패키지 변경

--- server/src/main/java/com/server/soopool/auth/SecurityConfig.java
+++ server/src/main/java/com/server/soopool/auth/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
