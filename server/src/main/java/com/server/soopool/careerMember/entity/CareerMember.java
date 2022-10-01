7890fcf1bcba64dd362f3ea772d803ace563ccd7
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:34:31 +0900

    CarrerLevelName Enum 추가

--- server/src/main/java/com/server/soopool/careerMember/entity/CareerMember.java
+++ server/src/main/java/com/server/soopool/careerMember/entity/CareerMember.java
@@ -20,9 +20,30 @@ public class CareerMember {
     // ManyToOne 설정
     @ManyToOne(targetEntity = Member.class)
     @JoinColumn(name = "member_id")
-    private Member member;
+    private Member memberId;
 
     @ManyToOne(targetEntity = Career.class)
     @JoinColumn(name = "career_id")
-    private Career career;
+    private Career careerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CareerLevelName careerLevelName;
+
+    public enum CareerLevelName{
+        BEGINNER("초보"),
+        INTERMEDIATE("중수"),
+        MASTER("고수");
+
+        @Getter
+        private final String name;
+
+        CareerLevelName(String name) {
+            this.name = name;
+        }
+
+        public String getName(){
+            return name;
+        }
+    }
 }
