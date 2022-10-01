e6e0f96dc7a400bc349b4ad7cdcfd397048d299e
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:27:26 +0900

    imageUri 변수 추가

--- server/src/main/java/com/server/soopool/techstack/request/UserInfoTechStackRequest.java
+++ server/src/main/java/com/server/soopool/techstack/request/UserInfoTechStackRequest.java
@@ -1,8 +1,11 @@
 package com.server.soopool.techstack.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserInfoTechStackRequest {
     private String name;
+    private String imageUri;
 }
