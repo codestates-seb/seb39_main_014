74f55d956d401f18b13cbf359777118f2463e8c6
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 01:38:46 +0900

    deleteAllByMemberId 메서드 추가 findByMemberId 메서드 추가

--- server/src/main/java/com/server/soopool/memberTechstack/repository/MemberTechStackRepository.java
+++ server/src/main/java/com/server/soopool/memberTechstack/repository/MemberTechStackRepository.java
@@ -1,7 +1,14 @@
 package com.server.soopool.memberTechstack.repository;
 
+import com.server.soopool.careerMember.entity.CareerMember;
+import com.server.soopool.member.entity.Member;
 import com.server.soopool.memberTechstack.entity.MemberTechStack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MemberTechStackRepository extends JpaRepository<MemberTechStack, Long> {
+    void deleteAllByMemberId(Long memberId);
+    List findByMemberId(Member member);
 }
