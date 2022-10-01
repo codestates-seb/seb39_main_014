545b3a1e1bc96c90d638551113996a82840cc71f
yeojoo1224 <yeojoo1224@naver.com>
2022-10-01 17:59:08 +0900

    Modify: 게시글 수정을 위한 메서드 추가

--- server/src/main/java/com/server/soopool/boardTechstack/service/BoardTechStackService.java
+++ server/src/main/java/com/server/soopool/boardTechstack/service/BoardTechStackService.java
@@ -1,39 +1,33 @@
 package com.server.soopool.boardTechstack.service;
 
-import com.server.soopool.boardTechstack.entity.BoardTechStack;
 import com.server.soopool.boardTechstack.repository.BoardTechStackRepository;
-import com.server.soopool.global.exception.BusinessLogicException;
-import com.server.soopool.global.exception.ExceptionCode;
+import com.server.soopool.techstack.entity.TechStack;
+import com.server.soopool.techstack.repository.TechStackRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class BoardTechStackService {
-
     private final BoardTechStackRepository boardTechStackRepository;
+    private final TechStackRepository techStackRepository;
 
-    public BoardTechStack save(BoardTechStack boardTechStack) {
-        return boardTechStackRepository.save(boardTechStack);
+    @Transactional
+    public void deleteBoardTechStacks(long boardId) {
+        boardTechStackRepository.deleteAllByBoardId(boardId);
     }
 
-
-    public void deleteALLByBoardId(long boardId) {
-        BoardTechStack boardTechStack = boardTechStackRepository.findByBoardId(boardId)
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.TECHSTACK_NOT_FOUND));
-        boardTechStackRepository.delete(boardTechStack);
+    public List<Map.Entry<String, Integer>> findPopularTechStack() {
+        Map<String, Integer> map = new HashMap<>();
+        for(long i = 1; i <= techStackRepository.findAll().size(); i++) {
+            TechStack techStack = techStackRepository.findById(i).orElseThrow();
+            map.put(techStack.getTechStackName(), boardTechStackRepository.findByTechStackId(i).size());
+        }
+        List<Map.Entry<String, Integer>> techStackUsingCounts = new LinkedList<>(map.entrySet());
+        techStackUsingCounts.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+        return techStackUsingCounts;
     }
-
-
-//    public List<BoardTechStack> findBoardTechStacks(long boardId) {
-//        return boardTechStackRepository.findByBoardId(boardId);
-//    }
-//
-//    public void deleteByBoardId(long boardId) {
-//        BoardTechStack boardTechStack =
-//    }
-
 }
