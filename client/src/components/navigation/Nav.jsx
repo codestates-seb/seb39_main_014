65735dcf18b9f38f5181bd33a5c454c83df42786
so0112 <tkddh0112@gmail.com>
2022-10-02 15:39:20 +0900

    Feat: 로그인 상태 반영

--- client/src/components/navigation/Nav.jsx
+++ client/src/components/navigation/Nav.jsx
@@ -5,6 +5,9 @@ import handleLogout from "../../api/handleLogout";
 
 import styled from "styled-components";
 
+// true일경우 로그인된 상태
+const isLogin = Boolean(localStorage.getItem("token"));
+
 function Nav() {
   return (
     <NavFrame>
@@ -31,17 +34,25 @@ function Nav() {
           </LeftMenu>
         </Logo>
 
-        <RightMenu>
-          <StyledLink to="/board/write">
-            <div>작성하기</div>
-          </StyledLink>
-          <StyledLink to="/login">
-            <div>로그인</div>
-          </StyledLink>
-          <div className="logout-button" onClick={handleLogout}>
-            로그아웃
-          </div>
-        </RightMenu>
+        {isLogin ? (
+          <RightMenu>
+            <StyledLink to="/board/write">
+              <div>작성하기</div>
+            </StyledLink>
+            <div className="logout-button" onClick={handleLogout}>
+              로그아웃
+            </div>
+          </RightMenu>
+        ) : (
+          <RightMenu>
+            <StyledLink to="/board/write">
+              <div>작성하기</div>
+            </StyledLink>
+            <StyledLink to="/login">
+              <div>로그인</div>
+            </StyledLink>
+          </RightMenu>
+        )}
 
         <GiHamburgerMenu className="hambuger" />
       </NavContainer>
@@ -121,10 +132,12 @@ const RightMenu = styled.div`
   margin-right: 0px;
 
   .logout-button {
+    text-decoration: none;
+    color: #5f5f5f;
+    margin-left: 20px;
     cursor: pointer;
 
     // 조건부 렌더링 넣으면 지울 내용
-    margin-left: 5px;
   }
 `;
 
