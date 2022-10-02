f2d2f567cc5a7a28eb2f88c50c2775410510023a
yeojoo1224 <yeojoo1224@naver.com>
2022-10-02 20:04:47 +0900

    Add: BoardApply entity 추가

--- server/src/main/java/com/server/soopool/boardApply/entity/BoardApply.java
+++ server/src/main/java/com/server/soopool/boardApply/entity/BoardApply.java
@@ -1,12 +1,11 @@
 package com.server.soopool.boardApply.entity;
 
+import com.server.soopool.boardCareer.entity.BoardCareer;
+import com.server.soopool.member.entity.Member;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,5 +15,28 @@ public class BoardApply {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(targetEntity = BoardCareer.class)
+    @JoinColumn(name = "board_career_id")
+    private BoardCareer boardCareer;
+
+    @ManyToOne(targetEntity = Member.class)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public void addBoardCareer(BoardCareer boardCareer) {
+        this.boardCareer = boardCareer;
+        if(!this.boardCareer.getBoardApplies().contains(this)) {
+            this.boardCareer.addBoardApply(this);
+        }
+    }
+
+    public void addMember(Member member) {
+        this.member = member;
+        if(!this.member.getBoardApplies().contains(this)) {
+            this.member.addBoardApply(this);
+        }
+    }
+
 
 }
