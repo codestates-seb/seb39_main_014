4f6882744c4985075e5e605687f3c89b60dbdb80
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-02 23:56:16 +0900

    AccessDeniedException 추가

--- server/src/main/java/com/server/soopool/global/exception/GlobalException.java
+++ server/src/main/java/com/server/soopool/global/exception/GlobalException.java
@@ -5,6 +5,7 @@ import com.server.soopool.global.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +23,10 @@ public class GlobalException {
     public ResponseEntity handleBusinessException(BusinessLogicException e){
         ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
         return new ResponseEntity<>(response, HttpStatus.valueOf(e.getExceptionCode().getStatus()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleAccessDeniedException(AccessDeniedException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
