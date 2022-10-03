ba8f993a6b5e7b8b8ebe6d0d5eaeb11665bf8e0d
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-04 02:31:15 +0900

    findByGroupNumber 메소드 추가, deleteByGroupNumber 메소드 추가

--- server/src/main/java/com/server/soopool/comment/repository/CommentRepository.java
+++ server/src/main/java/com/server/soopool/comment/repository/CommentRepository.java
@@ -7,12 +7,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<BoardIdAndGroupNumberMapping> findByBoardId(Long boardId);
+
+    Comment findByGroupNumber(Long groupNumber);
+
+    void deleteByGroupNumber(Long groupNumber);
 
     // boardId의 조건에 부합하는 모든 comments를 조회합니다.
     @Query(value = "SELECT c " +
@@ -29,7 +36,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     void updateComment(@Param("content") String content,
                        @Param("board") Board board,
                        @Param("member") Member member,
-                       @Param("groupNumber") Integer groupNumber);
+                       @Param("groupNumber") Long groupNumber);
 
     // board, member, groupNumber의 조건에 부합하는 Comment table의 row들을 조회합니다.
     @Query(value = "SELECT c " +
@@ -37,7 +44,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
             "WHERE c.board = :board AND c.member = :member AND c.groupNumber = :groupNumber")
     Comment getCommentMatchingMemberIdAndBoardIdAAndGroupNumber(@Param("board") Board board,
                @Param("member") Member member,
-               @Param("groupNumber") Integer groupNumber);
+               @Param("groupNumber") Long groupNumber);
 
     @Modifying(clearAutomatically = true)
     @Transactional
@@ -45,6 +52,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
             "WHERE c.board = :board AND c.member = :member AND c.groupNumber = :groupNumber")
     void deleteComment(@Param("board") Board board,
                        @Param("member") Member member,
-                       @Param("groupNumber") Integer groupNumber);
+                       @Param("groupNumber") Long groupNumber);
 
 }
