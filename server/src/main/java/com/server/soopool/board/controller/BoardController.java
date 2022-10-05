551f20bc9940c98e581abda5c2bbe9f9514db7b4
yeojoo1224 <yeojoo1224@naver.com>
2022-10-05 15:15:40 +0900

    Modify: 게시글 지원 취소, 지원 현황 추가

--- server/src/main/java/com/server/soopool/board/controller/BoardController.java
+++ server/src/main/java/com/server/soopool/board/controller/BoardController.java
@@ -6,10 +6,12 @@ import com.server.soopool.board.dto.BoardPostDto;
 import com.server.soopool.board.entity.Board;
 import com.server.soopool.board.mapper.BoardMapper;
 import com.server.soopool.board.service.BoardService;
+import com.server.soopool.boardApply.dto.BoardApplyPostDto;
 import com.server.soopool.boardApply.entity.BoardApply;
 import com.server.soopool.boardApply.repository.BoardApplyRepository;
 import com.server.soopool.boardCareer.entity.BoardCareer;
 import com.server.soopool.boardCareer.service.BoardCareerService;
+import com.server.soopool.boardTechstack.entity.BoardTechStack;
 import com.server.soopool.boardTechstack.service.BoardTechStackService;
 import com.server.soopool.global.dto.MultiResponseDto;
 import com.server.soopool.global.dto.SingleResponseDto;
@@ -26,6 +28,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Positive;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -148,7 +151,7 @@ public class BoardController {
             throw new BusinessLogicException(ExceptionCode.CAN_NOT_DELETE_BOARD);
         }
         boardService.deleteById(boardId);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity("게시글 삭제 처리되었습니다", HttpStatus.NO_CONTENT);
     }
 
 
@@ -158,11 +161,13 @@ public class BoardController {
         return boardTechStackService.findPopularTechStack().subList(0, 5);
     }
 
-    @GetMapping("/{board-id}/{career-id}/apply")
+
+    // 게시글 지원
+    @PostMapping("/{board-id}/apply")
     @Secured("ROLE_USER")
-    public void applyBoard(@AuthenticationPrincipal PrincipalDetails principal,
+    public ResponseEntity applyBoard(@AuthenticationPrincipal PrincipalDetails principal,
                            @PathVariable("board-id") @Positive long boardId,
-                           @PathVariable("career-id") int careerId) {
+                           @RequestBody BoardApplyPostDto boardApplyPostDto) {
         Member member = memberService.findByUserId(principal.getUsername());
         Board board = boardService.findBoard(boardId);
 
@@ -174,16 +179,16 @@ public class BoardController {
 
         List<BoardApply> boardApplies = member.getBoardApplies();
         for(BoardApply boardApply1 : boardApplies) {
-            if(boardApply1.getBoardCareer().getBoard().getId() == boardId) {
+            if (boardApply1.getBoardCareer().getBoard().getId() == boardId) {
                 throw new BusinessLogicException(ExceptionCode.ALREADY_RECRUIT);
+                }
             }
-        }
 
         List<BoardCareer> boardCareers = board.getBoardCareers();
 
         BoardCareer result = new BoardCareer();
         for(BoardCareer boardCareer : boardCareers) {
-            if(boardCareer.getCareer().getId() == careerId) {
+            if(boardCareer.getCareer().getId() == boardApplyPostDto.getCareerId()) {
                 result = boardCareer;
             }
         }
@@ -194,14 +199,64 @@ public class BoardController {
 
         boardApply.setBoardCareer(result);
         boardApply.setMember(member);
-        result.setCareerCurrentRecruit(result.getCareerCurrentRecruit() + 1);
-        board.setCurrentRecruit(board.getCurrentRecruit() + 1);
         if(board.getCurrentRecruit() == board.getTotalRecruit()) {
             board.setRecruitDone(true);
-            boardService.save(board);
         }
 
         boardApplyRepository.save(boardApply);
 
+        int count = 0;
+        for(BoardCareer boardCareer : boardCareers) {
+            if(boardCareer.getCareer().getId() == boardApplyPostDto.getCareerId()) {
+                boardCareer.setCareerCurrentRecruit(boardCareer.getBoardApplies().size());
+            }
+            count += boardCareer.getCareerCurrentRecruit();
+        }
+
+        board.setCurrentRecruit(count);
+        boardService.save(board);
+
+
+        return new ResponseEntity<>("게시글 신청이 완료되었습니다.", HttpStatus.CREATED);
     }
+
+
+    // 게시글 신청 취소
+    @DeleteMapping("/{board-id}/apply")
+    @Secured("ROLE_USER")
+    public ResponseEntity applyBoardCancel(@AuthenticationPrincipal PrincipalDetails principal,
+                           @PathVariable("board-id") @Positive long boardId,
+                           @RequestBody BoardApplyPostDto boardApplyPostDto) {
+
+        Member member = memberService.findByUserId(principal.getUsername());
+
+        List<BoardApply> boardApplies = member.getBoardApplies();
+        for(BoardApply boardApply1 : boardApplies) {
+            if (boardApply1.getBoardCareer().getBoard().getId() == boardId) {
+                boardApplyRepository.delete(boardApply1);
+            }
+        }
+
+        return new ResponseEntity<>("게시글 신청 취소가 완료되었습니다.", HttpStatus.NO_CONTENT);
+
+    }
+
+    // 게시글 신청 현황 조회
+    @GetMapping("/{board-id}/apply")
+    public ResponseEntity applyMember(@PathVariable("board-id") @Positive long boardId){
+        Board board = boardService.findBoard(boardId);
+        List<BoardCareer> boardCareers = board.getBoardCareers();
+        List<BoardApply> boardApplies = new ArrayList<>();
+        for(BoardCareer boardCareer: boardCareers) {
+            for(BoardApply boardApply: boardCareer.getBoardApplies()) {
+                boardApplies.add(boardApply);
+            }
+        }
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(boardMapper.boardApplyToBoardApplyResponse(boardApplies)),
+                HttpStatus.OK);
+
+    }
+
 }
