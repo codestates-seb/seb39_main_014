cfdf52390bd93947b67581fb30d9135ba997657f
so0112 <tkddh0112@gmail.com>
2022-10-05 17:33:52 +0900

    Feat: 드롭박스

--- client/src/components/navigation/Hambuger.jsx
+++ client/src/components/navigation/Hambuger.jsx
@@ -1,73 +1,40 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import styled from "styled-components";
 
-// /*
-
-// loginUser Component에서 DropDown bar button CLick시 rendering 되는 component로,
-// 작성 글, 찜한 글, 로그아웃으로 구성되어 있습니다.
-
-// 로그아웃의 경우 handleLogout을 통해 user를 초기화하는 과정을 진행하며
-// API를 통해 refresh token을 초기화 합니다.
-
-// */
-
-const Hambuger = () => {
+function Hambuger({ login }) {
   return (
-    <Li>
-      <DropDown>
-        <ListContainer>
-          <Ul>
-            <Li>
-              <Link to="/">프로필</Link>
-            </Li>
-            <Li>
-              <Link to="/">매니저</Link>
-            </Li>
-            <Li>
-              <Link>로그아웃</Link>
-            </Li>
-          </Ul>
-        </ListContainer>
-      </DropDown>
-    </Li>
+    <>
+      {login ? (
+        <>
+          <HambugerContainer>
+            <div>로그인</div>
+            <div>모집하기</div>
+            <div></div>
+          </HambugerContainer>
+        </>
+      ) : (
+        <HambugerContainer>
+          <div>마이페이지</div>
+          <div>모집하기</div>
+          <div>로그아웃</div>
+          <div></div>
+        </HambugerContainer>
+      )}
+    </>
   );
-};
+}
 
-const Li = styled.li`
-  list-style: none;
-`;
-
-const Ul = styled.ul`
-  list-style: none;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  color: #666666;
-  line-height: 22px;
-`;
-
-const DropDown = styled.button`
-  border: none;
-  outline: none;
-  position: relative;
-  width: 80px;
-`;
-
-const ListContainer = styled.div`
-  border: 1px solid ${(props) => props.theme.borderColor};
-  background-color: ${(props) => props.theme.bgColor};
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 5px;
-  padding: 3px;
-  margin-top: 7px;
+const HambugerContainer = styled.div`
+  width: 100px;
   position: absolute;
-  display: none;
-  ${DropDown}:active & {
-    display: block;
-  }
-  ${DropDown}:focus & {
-    display: block;
+  top: 50px;
+  right: 20px;
+  background-color: wheat;
+  border-radius: 16px;
+
+  div:hover {
+    color: blue;
+    background-color: green;
   }
 `;
 
