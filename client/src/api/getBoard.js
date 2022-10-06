24632bf8ca8faf510c96c72d099eee00110a620c
so0112 <tkddh0112@gmail.com>
2022-10-06 12:22:56 +0900

    Fix: 게시글 useEffect 오류 수정

--- client/src/api/getBoard.js
+++ client/src/api/getBoard.js
@@ -3,9 +3,9 @@ import axios from "axios";
 export default function getBoard(BOARD_URL, setState) {
   axios
     .get(BOARD_URL, {
-      // headers: {
-      //   Authorization: `Bearer ${localStorage.getItem("token")}`,
-      // },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
     })
     .then((res) => {
       setState(res.data.boards);
