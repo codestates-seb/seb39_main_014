f5798d23a79039fdb0218d3728798dcea3112b61
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-25 18:56:02 +0900

    Add : 게시판 DAO 구현

---
+++ server/src/main/java/com/server/soopool/communication/repository/CommunicationRepository.java
@@ -0,0 +1,13 @@
+package com.server.soopool.communication.repository;
+
+import com.server.soopool.communication.entity.Communication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommunicationRepository extends JpaRepository<Communication, Long> {
+}
