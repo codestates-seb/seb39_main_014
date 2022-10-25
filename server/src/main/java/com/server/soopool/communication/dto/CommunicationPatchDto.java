3c21b391139d8aa290d7c179508761a65a7c99b8
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-10-25 18:55:02 +0900

    Add : 게시판 Dto 구현

---
+++ server/src/main/java/com/server/soopool/communication/dto/CommunicationPatchDto.java
@@ -0,0 +1,12 @@
+package com.server.soopool.communication.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommunicationPatchDto {
+    private Long id;
+    private String title;
+    private String contents;
+}
---
+++ server/src/main/java/com/server/soopool/communication/dto/CommunicationPostDto.java
@@ -0,0 +1,11 @@
+package com.server.soopool.communication.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CommunicationPostDto {
+    private String title;
+    private String contents;
+}
