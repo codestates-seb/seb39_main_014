9a1c6e3fae73872eecb1f5b43213f3ccc3d4692e
hopak-e <a328028@gmail.com>
2022-11-21 16:30:58 +0900

    Refactor : API 관련 상수 및 path 관리하는 파일 추가

---
+++ client/src/api/common.ts
@@ -0,0 +1,22 @@
+export const httpMehthod = {
+  GET: "GET",
+  POST: "POST",
+  PATCH: "PATCH",
+  DELETE: "DELETE",
+} as const;
+
+export const ACCESS_TOKEN = "ACCESS_TOKEN";
+
+export const NICK_NAME = "NICK_NAME";
+
+export const API_PATH = {
+  auth: {
+    signup: "/signup",
+    login: "/login",
+    mypage: "mypage",
+  },
+  board: {
+    board: "/board",
+    write: "/write",
+  },
+};
