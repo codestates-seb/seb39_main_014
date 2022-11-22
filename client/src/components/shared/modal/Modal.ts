6b44611462649f5fa6806a682189f4b837a4033a
hopak-e <a328028@gmail.com>
2022-11-22 18:09:05 +0900

    Refactor : 확인, 성공 모달 창 함수로 적용하여 재사용

---
+++ client/src/components/shared/modal/Modal.ts
@@ -0,0 +1,22 @@
+import Swal from "sweetalert2";
+
+export const ConfirmModal = (title: string) => {
+  const res = Swal.fire({
+    title,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#69D06F",
+    cancelButtonColor: "#FF6464",
+    confirmButtonText: "확인",
+    cancelButtonText: "취소",
+  });
+  return res;
+};
+
+export const SuccessModal = (title: string) => {
+  Swal.fire({
+    title,
+    icon: "success",
+    confirmButtonColor: "#69D06F",
+  });
+};
