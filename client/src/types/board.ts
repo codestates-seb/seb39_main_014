ebb7a7f5bedb84860e0fa1e55ca6bf061dcf0be0
so0112 <tkddh0112@gmail.com>
2022-11-25 15:29:21 +0900

    Refacotr: handlefilter  ts 변환

---
+++ client/src/types/board.ts
@@ -0,0 +1,32 @@
+interface Career {
+  careerCurrentRecruit: number;
+  careerName: string;
+  careerTotalRecruit: number;
+}
+interface TechStack {
+  techStackName: string;
+}
+
+export interface Datas {
+  bookmarkCount: number;
+  commentAmount: number;
+  contact: string;
+  contents: string;
+  createdAt: string;
+  currentRecruit: number;
+  deleted: boolean;
+  deletedAt: null;
+  id: number;
+  location: string;
+  modifiedAt: string;
+  nickName: string;
+  period: string;
+  recruitCategory: string;
+  recruitDone: boolean;
+  recruitMethod: string;
+  title: string;
+  totalRecruit: number;
+  viewCount: number;
+  boardCareers: Array<Career>;
+  techStackNames: Array<TechStack>;
+}
